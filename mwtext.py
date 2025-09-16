29d433160958787d6e08875b58e31d0e70159fb1
mk1037 <marekm1037@gmail.com>
2025-09-16 18:46:32 +0200

    Lilcense comment for mwtext.py

--- mwtext.py
+++ mwtext.py
@@ -2,6 +2,23 @@
 
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2025 Marek Momot
+#
+# This file is part of mwtext
+#
+# mwtext is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# mwtext is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mwtext.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 import sys
 import re
